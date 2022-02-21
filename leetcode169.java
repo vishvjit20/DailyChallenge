b79f4bc5acc1a201f62131a3729adefde060e6e7
vishwajeet <singhvishwajeet1998@gmail.com>
2022-02-21 21:20:48 +0530

    update

---
+++ leetcode169.java
@@ -0,0 +1,16 @@
+public class leetcode169 {
+    public int majorityElement(int[] nums) {
+        int count = 0, res = 0;
+        for (int num : nums) {
+            if (count == 0) {
+                res = num;
+            }
+            if (num != res) {
+                count--;
+            } else {
+                count++;
+            }
+        }
+        return res;
+    }
+}
